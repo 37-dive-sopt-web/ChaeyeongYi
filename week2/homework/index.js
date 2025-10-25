d2ffa88d32bc1d68eeaa2b1dca616e41d9c0e528
Chaeyeong Lee <pop98149814@gmail.com>
2025-10-26 00:37:35 +0900

    feat: 검색 필터링 구현

--- week2/homework/index.js
+++ week2/homework/index.js
@@ -1,5 +1,7 @@
 import { memberMockData } from "./memberMockData.js";
 const STORAGE_KEY = "members";
+const MEMEBER_ID_KEY = "memberId";
+
 // 요소 선택
 const memberList = document.querySelector(".member-list tbody");
 
@@ -12,42 +14,50 @@ const geumjandiInput = document.querySelector(".geumjandi-input");
 const ageInput = document.querySelector(".age-input");
 const implementBtn = document.querySelector(".implement-btn");
 const resetBtn = document.querySelector(".reset-btn");
+
+const deleteBtn = document.querySelector(".delete-btn");
+const addBtn = document.querySelector(".add-btn");
 // -----------------------------------------------------------------
 
-// 1. 초기화: localStorage 데이터 get
+// localStorage 데이터 get
 let members = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let newId = localStorage.getItem(MEMEBER_ID_KEY);
 
-// TODO: localStorage 데이터 없을 경우, memeberMockData로 초기화 진행
-if (!members || members.length === 0) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(memberMockData));
-  members = [...memberMockData];
-}
+// TODO: table 데이터 업데이트
+const updateTable = (data = members) => {
+  memberList.innerHTML = "";
 
-// 테이블에 추가
-members.forEach((member) => {
-  const tr = document.createElement("tr");
-  tr.innerHTML = `
-    <td><input type="checkbox" /></td>
+  data.forEach((member) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td><input type="checkbox" member-id="${member.id}"/></td>
     <td>${member.name}</td>
     <td>${member.englishName}</td>
     <td>
       <a id="github" href="https://github.com/${member.github}">${
-    member.github
-  }</a>
+      member.github
+    }</a>
     </td>
     <td>${member.gender === "female" ? "여성" : "남성"}</td>
     <td>${member.role}</td>
     <td>${member.codeReviewGroup}</td>
     <td>${member.age}</td>
   `;
+    memberList.appendChild(tr);
+  });
+};
 
-  memberList.appendChild(tr);
-});
+// 0. 초기화
+if (!members || members.length === 0) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(memberMockData));
+  members = [...memberMockData];
+  localStorage.setItem(MEMEBER_ID_KEY, members.length + 1);
+  newId = members.length + 1;
+}
+updateTable();
 
-// TODO: 검색 필터 input 초기화
-resetBtn.addEventListener("click", resetFilter);
-
-function resetFilter() {
+// resetFilter: 검색 필터 input 초기화 함수
+const resetFilter = () => {
   nameInput.value = "";
   engNameInput.value = "";
   githubInput.value = "";
@@ -55,27 +65,56 @@ function resetFilter() {
   ageInput.value = "";
   genderSelect.selectedIndex = 0;
   roleSelect.selectedIndex = 0;
-}
+  return;
+};
+resetBtn.addEventListener("click", resetFilter);
 
-// TODO: 검색 필터 적용 핸들링
-implementBtn.addEventListener("click", implementFilter);
-
+// implementFilter: 검색 필터 적용 함수
 function implementFilter() {
+  // 1. 필터 값 가져오기
   const filters = {
     name: nameInput.value.trim(),
     englishName: engNameInput.value.trim(),
     github: githubInput.value.trim(),
-    gender: genderSelect.value,
-    role: roleSelect.value,
+    gender:
+      genderSelect.value.trim() === "entire" ? "" : genderSelect.value.trim(),
+    role: roleSelect.value.trim() === "entire" ? "" : roleSelect.value.trim(),
     codeReviewGroup: geumjandiInput.value.trim(),
     age: ageInput.value.trim(),
   };
 
-  console.log("Filter values:", filters);
+  // 2. 공백이 아닌 항목만 따로 저장
+  const finalFilter = {};
+  for (const key in filters) {
+    if (filters[key] !== "") {
+      finalFilter[key] = filters[key].toLowerCase();
+    }
+  }
 
-  return filters;
+  // 3. 필터링 적용
+  const applyFiltering = (mem, filter) => {
+    return Object.keys(filter).every((key) => {
+      const filtered = String(filter[key]);
+      const target =
+        mem[key] !== undefined && mem[key] !== null
+          ? String(mem[key]).toLowerCase()
+          : "";
+      return target.includes(filtered);
+    });
+  };
+
+  const filteredData = members.filter((member) => {
+    return applyFiltering(member, finalFilter);
+  });
+
+  // 4. 업데이트
+  updateTable(filteredData);
+  return;
 }
+implementBtn.addEventListener("click", implementFilter);
+
 // TODO: 체크 박스 선택 삭제
 // TODO: 체크 박스 전체 삭제
 // TODO: 데이터 추가
+// localStorage.setItem(MEMEBER_ID_KEY, newId);
 // TODO: 모달 관리
