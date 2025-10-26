6f513492b493cafb4f9fab9ab847b864c04b15fe
Chaeyeong Lee <pop98149814@gmail.com>
2025-10-26 17:53:30 +0900

    feat: 체크박스 전체 선택 및 멤버 추가 모달 구현

--- week2/homework/index.js
+++ week2/homework/index.js
@@ -1,7 +1,6 @@
 import { memberMockData } from "./memberMockData.js";
 const STORAGE_KEY = "members";
 const MEMEBER_ID_KEY = "memberId";
-
 // 요소 선택
 const memberList = document.querySelector(".member-list tbody");
 
@@ -14,15 +13,26 @@ const geumjandiInput = document.querySelector(".geumjandi-input");
 const ageInput = document.querySelector(".age-input");
 const implementBtn = document.querySelector(".implement-btn");
 const resetBtn = document.querySelector(".reset-btn");
-const checkAllInput = document.querySelector(".check-all");
+const checkAllBtn = document.querySelector(".check-all");
 const deleteBtn = document.querySelector(".delete-btn");
-const addBtn = document.querySelector(".add-btn");
-// -----------------------------------------------------------------
+const openAddModalBtn = document.querySelector(".open-modal-btn");
+
+// 모달 요소
+const modal = document.querySelector(".modal");
+const modalNameInput = document.querySelector(".modal-name-input");
+const modalEngNameInput = document.querySelector(".modal-eng-name-input");
+const modalGithubInput = document.querySelector(".modal-github-input");
+const modalGenderSelect = document.querySelector(".modal-gender-select");
+const modalRoleSelect = document.querySelector(".modal-role-select");
+const modalGeumjandiInput = document.querySelector(".modal-geumjandi-input");
+const modalAgeInput = document.querySelector(".modal-age-input");
+const modalCloseBtn = document.querySelector(".close-btn");
+const addRowBtn = document.querySelector(".add-row-btn");
 
 // localStorage 데이터 get
 let members = JSON.parse(localStorage.getItem(STORAGE_KEY));
 let newId = localStorage.getItem(MEMEBER_ID_KEY);
-
+// let isModalOpen =
 // TODO: table 데이터 업데이트
 const updateTable = (data = members) => {
   memberList.innerHTML = "";
@@ -70,7 +80,7 @@ const resetFilter = () => {
 resetBtn.addEventListener("click", resetFilter);
 
 // implementFilter: 검색 필터 적용 함수
-function implementFilter() {
+const implementFilter = () => {
   // 1. 필터 값 가져오기
   const filters = {
     name: nameInput.value.trim(),
@@ -110,18 +120,16 @@ function implementFilter() {
   // 4. 업데이트
   updateTable(filteredData);
   return;
-}
+};
 implementBtn.addEventListener("click", implementFilter);
 
-// TODO: 체크 박스 선택 삭제
+// deleteRow: 체크 박스 선택 삭제
 const deleteRow = () => {
-  // 1. 선택된 row의 id 저장
   const checkedBoxes = document.querySelectorAll(
     "td > input[type=checkbox]:checked"
   );
   const checkedIdList = [...checkedBoxes].map((item) => item.id);
 
-  // 2. 업데이트
   const deletedData = members.filter((item) => {
     return !checkedIdList.includes(item.id.toString());
   });
@@ -132,7 +140,49 @@ const deleteRow = () => {
 };
 deleteBtn.addEventListener("click", deleteRow);
 
-// TODO: 체크 박스 전체 선택
-// TODO: 데이터 추가
-// localStorage.setItem(MEMEBER_ID_KEY, newId);
-// TODO: 모달 관리
+// checkAllRow: 체크 박스 전체 선택
+const checkAllRow = () => {
+  const allCheckedBoxes = document.querySelectorAll(
+    "td > input[type=checkbox]"
+  );
+  allCheckedBoxes.forEach((it) => {
+    it.checked = checkAllBtn.checked;
+  });
+};
+checkAllBtn.addEventListener("change", checkAllRow);
+
+// TODO: 모달 오픈 이벤트
+openAddModalBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+});
+
+// TODO: 모달 닫기 이벤트
+const closeModal = () => {
+  modal.classList.remove("open");
+};
+modalCloseBtn.addEventListener("click", closeModal);
+// addRow: 데이터 추가
+const addRow = () => {
+  const newData = {
+    id: newId,
+    name: modalNameInput.value.trim(),
+    englishName: modalEngNameInput.value.trim(),
+    github: modalGithubInput.value.trim(),
+    gender: modalGenderSelect.value.trim(),
+    role: modalRoleSelect.value.trim(),
+    codeReviewGroup: modalGeumjandiInput.value.trim(),
+    age: modalAgeInput.value.trim(),
+  };
+
+  if (Object.values(newData).includes("")) {
+    alert("값을 모두 입력해주세요!");
+    console.log("newData:", newData);
+  } else {
+    console.log("newData:", newData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([...members, newData]));
+    localStorage.setItem(MEMEBER_ID_KEY, ++newId);
+    updateTable(members);
+    closeModal();
+  }
+};
+addRowBtn.addEventListener("click", addRow);
