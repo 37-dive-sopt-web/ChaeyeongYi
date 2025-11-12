64a78cc188ad271c60d367ce2161a5e712433bb6
hummingbbird <pop9814@kookmin.ac.kr>
2025-11-13 04:02:00 +0900

    init: 초기 세팅

---
+++ Week4/Homework/signup-project/src/router/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+// import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   Component: Home,
+  // },
+  //   {
+  //     path: '/pokemon/:name',
+  //     Component: PokemonDetail,
+  //   },
+]);
+
+export default router;
